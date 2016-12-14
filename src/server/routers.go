8e33cad2d54c256b4302eebdebf03f8f16ed4995
schoeu <quanyou1013@gmail.com>
2016-12-14 23:59:50 +0800

    update modules split

--- src/server/routers.go
+++ src/server/routers.go
@@ -4,7 +4,6 @@ import (
 	"gopkg.in/gin-gonic/gin.v1"
 	"net/http"
 	"path/filepath"
-	"fmt"
 )
 
 func routers(r *gin.Engine) {
@@ -13,21 +12,8 @@ func routers(r *gin.Engine) {
 	db := getDB()
     defer db.Close()
 
-	// Execute the query
-    rows, err := db.Query("SELECT * FROM vs_users")
-    if err != nil {
-        panic(err.Error()) 
-    }
-
-	// Get column names
-    columns, err := rows.Columns()
-    if err != nil {
-        panic(err.Error()) 
-    }
-
 	r.POST("/api/login", func(c *gin.Context) {
 		var form Login
-        // This will infer what binder to use depending on the content-type header.
         if c.Bind(&form) == nil {
             if form.User == "test" && form.Password == "123" {
                c.HTML(http.StatusOK, "main.tmpl", gin.H{
